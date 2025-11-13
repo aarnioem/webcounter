f1ee7aa73a75e7349bf94554f2dbd781a4b92aa9
aarnioem <aarnioem@gmail.com>
2025-11-13 14:40:36 +0200

    set value functionality

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from counter import Counter
 
 app = Flask(__name__)
@@ -16,4 +16,13 @@ def increment():
 @app.route("/reset", methods=["POST"])
 def reset():
     cnt.reset()
+    return redirect("/")
+
+@app.route("/set", methods=["POST"])
+def set_counter():
+    new_value = request.form.get("value")
+    try:
+        cnt.set_counter(int(new_value))
+    except TypeError:
+        return "Invalid value"
     return redirect("/")
